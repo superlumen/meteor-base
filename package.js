6e1c3f9b6b78f07fd8b66f2d8841075fd52f4d0d
Callum Macdonald <git@callum-macdonald.com>
2015-11-05 18:44:35 +0100

    Version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'superlumen:base',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Superlumen base package',
   // URL to the Git repository containing the source code for this package.
